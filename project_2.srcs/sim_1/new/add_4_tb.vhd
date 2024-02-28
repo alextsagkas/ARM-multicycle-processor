6789a2c4e82eae43243fd4e1a925604dc918f10f
Alexandros Tsagkaropoulos <alextsagkas2001@gmail.com>
2024-02-28 19:06:28 +0200

    transform add_4_tb to self-checking

--- project_2.srcs/sim_1/new/add_4_tb.vhd
+++ project_2.srcs/sim_1/new/add_4_tb.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use work.string_pkg.all;
 
 entity add_4_tb is
 end entity add_4_tb;
@@ -35,19 +36,70 @@ begin
   );
   -- Stimulus process
   stimuli : process
+    -- Procedure to report errors
+    procedure report_errors (
+      input_1          : in std_logic_vector (N_tb - 1 downto 0);
+      input_2          : in std_logic_vector (N_tb - 1 downto 0);
+      output_1         : in std_logic_vector (N_tb - 1 downto 0);
+      output_1_correct : in std_logic_vector (N_tb - 1 downto 0)) is
+    begin
+      if (output_1 /= output_1_correct) then
+        assert false
+        report
+          "input_1 = 0b" & to_bstring(input_1) & ", " &
+          "input_2 = 0b" & to_bstring(input_2) & " | " &
+          "output_1 = 0b" & to_bstring(output_1) & ", " &
+          "output_1_correct = 0b" & to_bstring(output_1_correct)
+          severity failure;
+      end if;
+    end procedure report_errors;
+
+    -- Apply test
+    procedure test (
+      input_1          : in std_logic_vector (N_tb - 1 downto 0);
+      input_2          : in std_logic_vector (N_tb - 1 downto 0);
+      output_1_correct : in std_logic_vector (N_tb - 1 downto 0)) is
+    begin
+      wait for 10 ns;
+      input_1_tb <= input_1;
+      input_2_tb <= input_2;
+
+      wait for 15 ns; -- get the timing simulation time to settle the signals
+      report_errors(
+      input_1          => input_1,
+      input_2          => input_2,
+      output_1         => output_1_tb,
+      output_1_correct => output_1_correct);
+    end procedure test;
   begin
     wait for 100 ns;
-    input_1_tb <= "00000000000000000000000000000000";
-    input_2_tb <= "00000000000000000000000000000100";
-    wait for 10 ns;
-    input_1_tb <= "00000000000000000000000000000001";
-    input_2_tb <= "00000000000000000000000000000100";
-    wait for 10 ns;
-    input_1_tb <= "00000000000000000000000000000010";
-    input_2_tb <= "00000000000000000000000000000100";
-    wait for 10 ns;
-    input_1_tb <= "00000000000000000000000000000011";
-    input_2_tb <= "00000000000000000000000000000100";
+
+    test (
+    input_1          => "00000000000000000000000000000000",
+    input_2          => "00000000000000000000000000000100",
+    output_1_correct => "00000000000000000000000000000100"
+    );
+
+    test (
+    input_1          => "00000000000000000000000000000001",
+    input_2          => "00000000000000000000000000000100",
+    output_1_correct => "00000000000000000000000000000101"
+    );
+
+    test (
+    input_1          => "00000000000000000000000000000010",
+    input_2          => "00000000000000000000000000000100",
+    output_1_correct => "00000000000000000000000000000110"
+    );
+
+    test (
+    input_1          => "00000000000000000000000000000011",
+    input_2          => "00000000000000000000000000000100",
+    output_1_correct => "00000000000000000000000000000111"
+    );
+
+    report "Testbench finished successfully!";
+
     wait;
   end process stimuli;
 end architecture sim;
